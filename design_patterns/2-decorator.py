6a35df7cf3c7f0e6d0f57e792e9bea344b09b927
Rmxpvl <remy.pinville@gmail.com>
2026-05-22 18:28:04 +0200

    add decorator pattern implementation for beverage toppings

---
+++ design_patterns/2-decorator.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""Decorator pattern example with beverage toppings."""
+
+
+class Beverage:
+    """Base beverage interface."""
+
+    def cost(self):
+        raise NotImplementedError
+
+    def description(self):
+        raise NotImplementedError
+
+
+class Coffee(Beverage):
+    """Plain coffee."""
+
+    def cost(self):
+        return 50
+
+    def description(self):
+        return "Coffee"
+
+
+class MilkDecorator(Beverage):
+    """Add milk to a beverage."""
+
+    def __init__(self, inner):
+        self._inner = inner
+
+    def cost(self):
+        return self._inner.cost() + 10
+
+    def description(self):
+        return self._inner.description() + " + milk"
+
+
+class SugarDecorator(Beverage):
+    """Add sugar to a beverage."""
+
+    def __init__(self, inner):
+        self._inner = inner
+
+    def cost(self):
+        return self._inner.cost() + 5
+
+    def description(self):
+        return self._inner.description() + " + sugar"
+
+
+class CaramelDecorator(Beverage):
+    """Add caramel to a beverage."""
+
+    def __init__(self, inner):
+        self._inner = inner
+
+    def cost(self):
+        return self._inner.cost() + 15
+
+    def description(self):
+        return self._inner.description() + " + caramel"
+
+
+def main():
+    beverage = Coffee()
+    print(beverage.description(), beverage.cost())
+
+    beverage = MilkDecorator(Coffee())
+    print(beverage.description(), beverage.cost())
+
+    beverage = CaramelDecorator(MilkDecorator(SugarDecorator(Coffee())))
+    print(beverage.description(), beverage.cost())
+
+
+if __name__ == "__main__":
+    main()
